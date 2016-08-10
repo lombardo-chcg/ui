077f85289ad72c88b8f26255a70a8e2082ff4ebf
lombardo-chcg <dpa.lombardo@gmail.com>
2016-08-10 07:28:16 -0500

    services

--- ui-basics/scripts/app.js
+++ ui-basics/scripts/app.js
@@ -1,13 +1,37 @@
 angular.module("todoListApp", [])
-	.controller("mainCtrl", function($scope) {
-		$scope.learningNgChange = function() {
-			console.log("Ng Change");
-		}
+	.controller("mainCtrl", function($scope, dataService) {
+		$scope.addToDo = function() {
+			var todo = {"name": "This is a new todo"};
+			$scope.todos.push(todo);
+		};
 
-		$scope.todos = [
-			{"name": "eat a taco"},
-			{"name": "talk to paco"},
-			{"name": "listen to jaco"},
-			{"name": "think of more rhymes"},
-		]
+		dataService.getToDos(function(response) {
+			console.log(response.data);
+			$scope.todos = response.data;
+		});
+
+		$scope.deleteToDo = function(toDo) {
+			dataService.deleteToDo(toDo);
+		};
+
+		$scope.saveToDo = function(toDo) {
+			dataService.saveToDo(toDo);
+		};		
+
+	}).service("dataService", function($http) {
+		
+		this.getToDos = function(callback) {
+			$http.get("http://localhost:9090/scripts/mock/todos.json")
+			.then(callback)
+		};
+
+		this.deleteToDo = function(toDo, $index) {
+			console.log(toDo.name, "has been deleted");
+			$scope.todos.splice($index, 1);
+		};
+
+		this.saveToDo = function(toDo) {
+			console.log(toDo.name, "has been saved");
+		};
+
 	});

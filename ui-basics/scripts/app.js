ff973e80675e14b2195388e395b2aea8049cd584
lombardo-chcg <dpa.lombardo@gmail.com>
2016-08-09 21:13:27 -0500

    add basic directives, prepare for iteration

--- ui-basics/scripts/app.js
+++ ui-basics/scripts/app.js
@@ -1,1 +1,13 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+	.controller("mainCtrl", function($scope) {
+		$scope.helloWorld = function() {
+			console.log("Hello world mainCtrl");
+		}
+
+		$scope.todos = [
+			{"name": "eat a taco"},
+			{"name": "talk to paco"},
+			{"name": "listen to jaco"},
+			{"name": "think of more rhymes"},
+		]
+	});

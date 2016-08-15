d15f70a0e71b750ee505b42ebc106272f73b9e30
lombardo-chcg <dpa.lombardo@gmail.com>
2016-08-14 22:10:50 -0500

    Basic router

--- components/scripts/app.js
+++ components/scripts/app.js
@@ -1,1 +1,7 @@
-angular.module("movies", []);
+angular.module("movies", ["ngRoute"])
+	.config(function($routeProvider) {
+		$routeProvider
+			.when("/list", { template: "<movie-list></movie-list>" })
+			.when("/about", { template: "about"})
+			.otherwise({ redirectTo: "/list"})
+	});

c04fc26342f21bd1e5df024589d812de72776770
lombardo-chcg <dpa.lombardo@gmail.com>
2016-08-21 21:56:20 -0500

    finish problem set

--- js-good-parts/problems.js
+++ js-good-parts/problems.js
@@ -146,7 +146,6 @@ console.log(x);
 
 // write demethodize, a function that converts a method to a binary function
 // reference https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
-console.log('here');
 
 function demethodize(func) {
   return function(thisArg, num2) {
@@ -156,3 +155,152 @@ function demethodize(func) {
 
 var x = demethodize(Number.prototype.add)(5,6);
 console.log(x);
+
+// write a function twice that takes a binary function and returns
+// a unary function that passes its argument to the binary function twice
+
+function twice(func) {
+  return function(num) {
+    return func(num, num);
+  };
+}
+
+var double = twice(add);
+var x = double(11); // => 22
+console.log(x);
+
+var square = twice(mul);
+var y = square(11);
+console.log(y); // => 121
+
+// write a function composeU that takes 2 unary functions
+// and returns a unary function that calls them both
+
+function composeU(func1, func2) {
+  return function(num) {
+    return func2(func1(num));
+  }
+}
+
+var x = composeU(double, square)(3) // => 36
+console.log(x);
+
+// write a function composeB that takes two binary functions
+// and returns a function that calls them both
+
+function composeB(func1, func2) {
+  return function(num1, num2, num3) {
+    return func2(func1(num1, num2), num3);
+  };
+}
+
+var x = composeB(add, mul)(2,3,5) // => 25
+console.log(x)
+
+// write a function that allows another function to be called only once
+
+function once(func) {
+  var counter = 0;
+  return function(num1, num2) {
+    if (counter < 1) {
+      counter += 1;
+      return func(num1, num2);
+    } else {
+      throw "FAMILY FEUD BUZZER SOUND"
+    }
+  };
+}
+//
+// function once(func) {
+//   return function() {
+//     var f = func;
+//     func = null;
+//     return f.apply(this, arguments);
+//   }
+// }
+
+add_once = once(add);
+var x = add_once(3,4); // => 7
+console.log(x);
+// var y = add_once(3,4); // => throw!
+
+// write a factory function that returns 2 functions
+// that implement an up/down counter
+
+function CounterConstructor(num) {
+  this.counter = num;
+
+  this.inc = function() {
+    this.counter += 1;
+    return this.counter;
+  };
+
+  this.dec = function() {
+    this.counter -= 1;
+    return this.counter;
+  };
+}
+
+function counterf(num) {
+  return new CounterConstructor(num);
+}
+//
+// function counterf(value) {
+//   return {
+//     inc: function() {
+//       value += 1;
+//       return value;
+//     },
+//     dec: function() {
+//       value -= 1;
+//       return value;
+//     }
+//   }
+// }
+
+var counter = counterf(10);
+console.log(counter.inc());
+console.log(counter.dec());
+
+
+// make a revokable function
+// that takes a "nice" function
+// and returns a "revoke" function
+// that denies access to the nice function
+// and an "invoke" function that can invoke the
+// nice function until it is revoked
+
+function revokable(func) {
+  return {
+    valid: true,
+
+    invoke: function(arg) {
+      if (this.valid === true) {
+        return func(arg);
+      } else {
+        throw "ERROR";
+      }
+    },
+
+    revoke: function() {
+      this.valid = false;
+    }
+  }
+}
+
+// function revokable(func) {
+//   return {
+//     invoke: function(arg) {
+//       return func(arg);
+//     },
+//     revoke: function() {
+//       func = null;
+//     }
+//   }
+// }
+
+temp = revokable(console.log);
+temp.invoke(7); // => logs 7
+temp.revoke();
+console.log('here');
+temp.invoke(7); // => throws!
